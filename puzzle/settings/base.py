62d1d234d55d262c27b754bcc67f648df0f7d9bf
Kremor Pärt <hello@kremor.xyz>
2020-01-11 13:24:23 -0600

    Update static files url

--- puzzle/settings/base.py
+++ puzzle/settings/base.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/puzzle/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
