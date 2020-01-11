f6b7125feb39c22f959e5cde591972a9e74a66b5
Kremor Pärt <hello@kremor.xyz>
2020-01-11 13:29:34 -0600

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,95 @@
-const path = require("path");
-const BundleTracker = require("webpack-bundle-tracker");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const path = require('path')
+const BundleTracker = require('webpack-bundle-tracker')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = [
   // JS
   {
-    target: "web",
-    entry: path.resolve(__dirname, "assets/js/index.js"),
+    target: 'web',
+    entry: {
+      admin: path.resolve(__dirname, 'assets/js/admin.js'),
+      editor: path.resolve(__dirname, 'assets/js/editor.js'),
+      login: path.resolve(__dirname, 'assets/js/login.js'),
+      page: path.resolve(__dirname, 'assets/js/page.js'),
+    },
     output: {
-      chunkFilename: "[name].bundle.js",
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "assets/bundles/js"),
-      publicPath: "/static/bundles/js/"
+      chunkFilename: '[name].bundle.js',
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'assets/bundles/js'),
+      publicPath: '/puzzle/static/bundles/js/',
     },
     resolve: {
       alias: {
-        vue$: "vue/dist/vue.esm.js"
+        vue$: 'vue/dist/vue.esm.js',
       },
-      extensions: [".js", ".json", ".vue"]
+      extensions: ['.js', '.json', '.vue'],
     },
     module: {
       rules: [
         {
           test: /\.css$/,
-          use: ["vue-style-loader", "css-loader"]
+          use: ['vue-style-loader', 'css-loader'],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"]
-            }
-          }
+              presets: ['@babel/preset-env'],
+            },
+          },
         },
         {
           test: /\.vue$/,
           exclude: /node_modules/,
-          use: "vue-loader"
-        }
-      ]
+          use: 'vue-loader',
+        },
+      ],
     },
     plugins: [
       new BundleTracker({
-        filename: "stats.js.json",
-        path: __dirname
+        filename: 'stats.js.json',
+        path: __dirname,
       }),
-      new VueLoaderPlugin()
+      new VueLoaderPlugin(),
     ],
-    mode: "development"
+    mode: 'development',
   },
   // SCSS
   {
-    entry: path.resolve(__dirname, "assets/scss/main.scss"),
+    entry: {
+      admin: path.resolve(__dirname, 'assets/scss/admin.scss'),
+      editor: path.resolve(__dirname, 'assets/scss/editor.scss'),
+      login: path.resolve(__dirname, 'assets/scss/login.scss'),
+      page: path.resolve(__dirname, 'assets/scss/page.scss'),
+    },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "assets/bundles/css")
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'assets/bundles/css'),
     },
     resolve: {
-      extensions: [".scss"]
+      extensions: ['.scss'],
     },
     module: {
       rules: [
         {
           test: /\.scss$/,
           exclude: /node_modules/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-        }
-      ]
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+      ],
     },
     plugins: [
       new BundleTracker({
-        filename: "stats.css.json",
-        path: __dirname
+        filename: 'stats.css.json',
+        path: __dirname,
       }),
       new MiniCssExtractPlugin({
-        filename: "[name].bundle.css"
-      })
+        filename: '[name].bundle.css',
+      }),
     ],
-    mode: "development"
-  }
-];
+    mode: 'development',
+  },
+]
