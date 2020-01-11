f1f9480bcb879a15f768b039de42bbdf120b228d
Kremor Pärt <hello@kremor.xyz>
2020-01-11 15:42:09 -0600

    Update login view

---
+++ assets/js/components/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    <div class="form">
+      <div class="field">
+        <input type="text" placeholder="Email or Username" />
+      </div>
+
+      <div class="field">
+        <input type="password" placeholder="Password" />
+      </div>
+
+      <div class="field">
+        <button>Login</button>
+      </div>
+    </div>
+
+    <div class="image" />
+  </div>
+</template>
---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Login from 'components/login'
+
+new Vue({
+  components: { Login },
+  el: '#login',
+  template: '<Login />',
+})
