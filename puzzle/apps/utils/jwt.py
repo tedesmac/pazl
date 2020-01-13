733e499bf005b2a9815ce57becf729fd84523984
Kremor Pärt <hello@kremor.xyz>
2020-01-13 11:01:30 -0600

    Update uitls/jwt.py

--- puzzle/apps/utils/jwt.py
+++ puzzle/apps/utils/jwt.py
@@ -14,7 +14,7 @@ def generate(user: User) -> str:
         },
         settings.SECRET_KEY,
         algorithm='HS512'
-    ).decode('utf-8')
+    )
     return token
 
 
@@ -32,8 +32,10 @@ def validate(token: str) -> Union[User, None]:
     except:
         return None
 
+    print(payload)
+
     try:
-        user = User.objects.get(id=payload.user)
+        user = User.objects.get(id=payload['user'])
     except User.DoesNotExist:
         user = None
 
