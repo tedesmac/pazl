fcb0d64bd7175952b7ce3134603a5055b3ea5082
Kremor Pärt <hello@kremor.xyz>
2020-01-13 16:39:49 -0600

    Add website model

--- puzzle/apps/website/models.py
+++ puzzle/apps/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Website(models.Model):
+    description = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'puzzle_website'
