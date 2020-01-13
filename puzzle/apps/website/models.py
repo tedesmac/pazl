0a18f7d3e6b638f8efcaa19f622a93d14419e29d
Kremor Pärt <hello@kremor.xyz>
2020-01-13 17:00:22 -0600

    Update Website model

--- puzzle/apps/website/models.py
+++ puzzle/apps/website/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Website(models.Model):
-    description = models.CharField(max_length=200)
-    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, blank=True, null=False)
+    name = models.CharField(max_length=50, blank=True, null=False)
 
     class Meta:
         db_table = 'puzzle_website'
