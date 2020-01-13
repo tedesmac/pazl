ae163c50517ebd57ca44f51f9f48da6292dab54c
Kremor Pärt <hello@kremor.xyz>
2020-01-13 10:41:11 -0600

    Add client api

---
+++ assets/js/api/index.js
@@ -0,0 +1,19 @@
+import Axios from 'axios'
+
+const TOKEN_NAME = 'puzzle_token'
+
+/**
+ */
+export const authenticate = (credentials = {}) => {
+  const token = window.localStorage.getItem(TOKEN_NAME)
+  return Axios.post('/puzzle/login/', { ...credentials, token })
+    .then(response => {
+      const token = response.data.token
+      console.log('token =>', token)
+      window.localStorage.setItem(TOKEN_NAME, token)
+      return true
+    })
+    .catch(error => {
+      return false
+    })
+}
