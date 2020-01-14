8fe3e83270cfed46c914a492718361b7ab1d361b
Kremor Pärt <hello@kremor.xyz>
2020-01-14 16:14:58 -0600

    Update decorators

--- puzzle/apps/utils/decorators.py
+++ puzzle/apps/utils/decorators.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect
 
 
@@ -5,5 +6,13 @@ def private(view):
     def make_private(request, *args, **kwargs):
         if request.is_authenticated:
             return view(request, *args, **kwargs)
-        return redirect('login')
+        return JsonResponse({}, status=403)
     return make_private
+
+
+def requires_login(view):
+    def wrapper(request, *args, **kwargs):
+        if request.is_authenticated:
+            return view(request, *args, **kwargs)
+        return redirect('login')
+    return wrapper
