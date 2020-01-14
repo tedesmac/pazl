a0c4a56434ff55664f5fe9418d95118ae4d99418
Kremor Pärt <hello@kremor.xyz>
2020-01-14 16:44:56 -0600

    Update settings profile

--- assets/js/components/admin/settings-profile.vue
+++ assets/js/components/admin/settings-profile.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    Settings Profile
+    <div class="section is-horizontal">
+      <div class="a-third form is-vertical subsection">
+        <div class="logo-big" />
+
+        <div class="field">
+          <div class="justify-center">
+            <button class="is-cyan">Pick</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="form is-vertical two-thirds subsection">
+        <div class="field">
+          <label>Name</label>
+          <input type="text" />
+        </div>
+
+        <div class="field">
+          <label>Description</label>
+          <textarea></textarea>
+        </div>
+
+        <div class="field">
+          <div class="justify-right">
+            <button class="is-cyan">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
