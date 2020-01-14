ff4baf21a864aac4977624008c6b29ea8bd0f752
Kremor Pärt <hello@kremor.xyz>
2020-01-14 16:13:37 -0600

    Update site pai

--- puzzle/apps/website/views.py
+++ puzzle/apps/website/views.py
@@ -1,28 +1,28 @@
+from django.http import JsonResponse
 from django.utils.decorators import method_decorator
+from django.views.generic import View
 from puzzle.apps.utils.decorators import private
 from puzzle.apps.website.models import Website
 from puzzle.apps.website.serializers import WebsiteSerializer
 from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
-from rest_framework.views import APIView
 
 
-class SiteAPI(APIView):
+class SiteAPI(View):
 
     @method_decorator(private)
     def get(self, req):
         website, _ = Website.objects.get_or_create(id=1)
-        return Response({
+        return JsonResponse({
             'description': website.description,
             'name': website.name
         })
 
     @method_decorator(private)
     def put(self, req):
+        website, _ = Website.objects.get_or_create(id=1)
         data = JSONParser().parse(req)
-        serializer = WebsiteSerializer(data=data)
+        serializer = WebsiteSerializer(website, data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response()
-        return Response(status=HTTP_400_BAD_REQUEST)
+            return JsonResponse({})
+        return JsonResponse({}, status=400)
