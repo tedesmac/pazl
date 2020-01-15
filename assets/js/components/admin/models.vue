11c3fb5db43e0016af6df06e2bb41422cbdf0218
Kremor Pärt <hello@kremor.xyz>
2020-01-15 14:07:19 -0600

    Update admin models

--- assets/js/components/admin/models.vue
+++ assets/js/components/admin/models.vue
@@ -3,9 +3,21 @@
     <div class="section is-horizontal space-around">
       <h2>Models</h2>
 
-      <button class="is-cyan">New</button>
+      <button class="is-cyan" @click="onNewModel">
+        New
+      </button>
     </div>
 
     <div class="section"></div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onNewModel() {
+      window.location = '/puzzle/editor/model?op=new'
+    },
+  },
+}
+</script>
