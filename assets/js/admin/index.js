756d6455a55cefe1d8677a79dc6310c9f5021a18
Kremor Pärt <hello@kremor.xyz>
2020-01-15 15:29:48 -0600

    Use app wrapper in admin

--- assets/js/admin/index.js
+++ assets/js/admin/index.js
@@ -1,10 +1,10 @@
-import Admin from 'components/admin'
+import App from 'components/app'
 import { rootComponent } from 'utils'
 import Router from './router'
 
 rootComponent({
-  components: { Admin },
-  el: '#admin',
+  components: { App },
+  el: '#app',
   router: Router(),
-  template: '<Admin />',
+  template: '<App />',
 })
--- assets/js/admin/router.js
+++ assets/js/admin/router.js
@@ -1,74 +1,112 @@
+import Admin from 'components/admin'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
+
 const Blocks = () =>
-  import(/* webpackChunkName: 'adminBlocks' */ 'components/admin/blocks')
+  import(
+    /* webpackChunkName: 'adminBlocks' */
+    'components/admin/blocks'
+  )
 const Images = () =>
-  import(/* webpackChunkName: 'adminImages' */ 'components/admin/images')
+  import(
+    /* webpackChunkName: 'adminImages' */
+    'components/admin/images'
+  )
 const Entries = () =>
-  import(/* webpackChunkName: 'adminEntries' */ 'components/admin/entries')
+  import(
+    /* webpackChunkName: 'adminEntries' */
+    'components/admin/entries'
+  )
 const Models = () =>
-  import(/* webpackChunkName: 'adminModels' */ 'components/admin/models')
+  import(
+    /* webpackChunkName: 'adminModels' */
+    'components/admin/models'
+  )
+const NotFound = () =>
+  import(
+    /* webpackChunkName: '404' */
+    'components/404'
+  )
 const Pages = () =>
-  import(/* webpackChunkName: 'adminPages' */ 'components/admin/pages')
+  import(
+    /* webpackChunkName: 'adminPages' */
+    'components/admin/pages'
+  )
 const SettingsProfile = () =>
   import(
-    /* webpackChunkName: 'adminSettingsProfile' */ 'components/admin/settings-profile'
+    /* webpackChunkName: 'adminSettingsProfile' */
+    'components/admin/settings-profile'
   )
 const SettingsSite = () =>
   import(
-    /* webpackChunkName: 'adminSettingsSite' */ 'components/admin/settings-site'
+    /* webpackChunkName: 'adminSettingsSite' */
+    'components/admin/settings-site'
   )
 const SettingsUsers = () =>
   import(
-    /* webpackChunkName: 'adminSettingsUsers' */ 'components/admin/settings-users'
+    /* webpackChunkName: 'adminSettingsUsers' */
+    'components/admin/settings-users'
   )
-
-Vue.use(VueRouter)
 
 const routes = [
   {
-    component: Blocks,
-    path: '/puzzle/admin/blocks',
-    name: 'blocks',
+    component: Admin,
+    path: '/puzzle/admin',
+    children: [
+      {
+        component: Blocks,
+        path: 'blocks',
+        name: 'blocks',
+      },
+      {
+        component: Images,
+        path: 'images',
+        name: 'images',
+      },
+      {
+        component: Entries,
+        path: 'entries/:model',
+        name: 'entries',
+        props: route => {
+          model: route.params.model
+        },
+      },
+      {
+        component: Models,
+        path: 'models',
+        name: 'models',
+      },
+      {
+        component: Pages,
+        path: 'pages',
+        name: 'pages',
+      },
+      {
+        component: SettingsProfile,
+        path: 'settings/profile',
+        name: 'settingsProfile',
+      },
+      {
+        component: SettingsSite,
+        path: 'settings/site',
+        name: 'settingsSite',
+      },
+      {
+        component: SettingsUsers,
+        path: 'settings/users',
+        name: 'settingsUsers',
+      },
+    ],
   },
   {
-    component: Images,
-    path: '/puzzle/admin/images',
-    name: 'images',
-  },
-  {
-    component: Entries,
-    path: '/puzzle/admin/entries/:model',
-    name: 'entries',
-    props: route => {
-      model: route.params.model
+    component: NotFound,
+    path: '/puzzle/admin/*',
+    name: 'notFound',
+    props: {
+      admin: true,
     },
-  },
-  {
-    component: Models,
-    path: '/puzzle/admin/models',
-    name: 'models',
-  },
-  {
-    component: Pages,
-    path: '/puzzle/admin/pages',
-    name: 'pages',
-  },
-  {
-    component: SettingsProfile,
-    path: '/puzzle/admin/settings/profile',
-    name: 'settingsProfile',
-  },
-  {
-    component: SettingsSite,
-    path: '/puzzle/admin/settings/site',
-    name: 'settingsSite',
-  },
-  {
-    component: SettingsUsers,
-    path: '/puzzle/admin/settings/users',
-    name: 'settingsUsers',
   },
 ]
 
