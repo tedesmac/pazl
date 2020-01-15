03b31331e66cacf9ba2d78f59db23de44c699cd7
Kremor Pärt <hello@kremor.xyz>
2020-01-15 15:18:52 -0600

    Add 404 component

---
+++ assets/js/components/404/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="__404">
+    <h1>404</h1>
+    <h2>This page doesn't exist</h2>
+
+    <div class="links">
+      <a href="/">
+        <FontAwesomeIcon :icon="faHome" />
+        Go to Home
+      </a>
+      <a href="/puzzle/admin/">
+        <FontAwesomeIcon :icon="faTools" />
+        Go to Admin
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHome, faTools } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  components: { FontAwesomeIcon },
+
+  computed: {
+    faHome: () => faHome,
+    faTools: () => faTools,
+  },
+}
+</script>
