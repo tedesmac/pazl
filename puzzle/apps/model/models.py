ccb6bc2daecf0bcf441a9c23cd77358d98105c5a
Kremor Pärt <hello@kremor.xyz>
2020-01-15 10:58:45 -0600

    Add new model (puzzle) Model

--- puzzle/apps/model/models.py
+++ puzzle/apps/model/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Model(models.Model):
+    data = models.TextField()
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'puzzle_model'
