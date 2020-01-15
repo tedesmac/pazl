0a5eb4336c90b49c752328297eac83ee43a48183
Kremor Pärt <hello@kremor.xyz>
2020-01-15 14:54:44 -0600

    Rename requires_login decorator to login_required

--- puzzle/utils/decorators.py
+++ puzzle/utils/decorators.py
@@ -2,17 +2,19 @@ from django.http import JsonResponse
 from django.shortcuts import redirect
 
 
-def private(view):
-    def make_private(request, *args, **kwargs):
-        if request.is_authenticated:
-            return view(request, *args, **kwargs)
-        return JsonResponse({}, status=403)
-    return make_private
-
-
-def requires_login(view):
+def login_required(view):
+    """Redirects to login view if user is not loged"""
     def wrapper(request, *args, **kwargs):
         if request.is_authenticated:
             return view(request, *args, **kwargs)
         return redirect('login')
     return wrapper
+
+
+def private(view):
+    """Responds with a 403 code (forbidden) if user doesn't have permissions"""
+    def make_private(request, *args, **kwargs):
+        if request.is_authenticated:
+            return view(request, *args, **kwargs)
+        return JsonResponse({}, status=403)
+    return make_private
