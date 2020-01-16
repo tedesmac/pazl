023051c5246d0878a0f0bbc765dcecf34acbd8ab
Kremor Pärt <hello@kremor.xyz>
2020-01-16 13:21:59 -0600

    Check model before saving

--- assets/js/components/editor/model.vue
+++ assets/js/components/editor/model.vue
@@ -59,6 +59,8 @@
         </div>
       </Draggable>
     </Workspace>
+
+    <Error :message="errorMessage" :show="error" @close="error = false" />
   </Editor>
 </template>
 
@@ -92,8 +94,9 @@ export default {
   data() {
     return {
       name: '',
-      saving: false,
       defaultBlocks,
+      modelId: 0,
+      saving: false,
     }
   },
 
@@ -111,8 +114,83 @@ export default {
     },
 
     onSave() {
-      this.saving = !this.saving
+      this.verifyAll()
+      if (!this.error) {
+        this.saving = true
+        this.$api.models
+          .post({
+            data: {
+              blocks: this.blocks,
+            },
+            name: this.name,
+          })
+          .then(data => {
+            console.log('[model saved] =>', data)
+            this.blocks = data.data.blocks
+            this.modelId = data.id
+            this.name = data.name
+            this.saving = false
+          })
+          .catch(() => {
+            this.saving = false
+            this.error = true
+            this.errorMessage = 'Unable to save, please try again later'
+          })
+      }
     },
+
+    verifyAll() {
+      if (!this.verifyBlocksNamed()) {
+        this.error = true
+        this.errorMessage = 'All blocks must be named'
+      } else if (!this.verifyNamed()) {
+        this.error = true
+        this.errorMessage = 'Model must be named'
+      } else if (!this.verifyUniqueNames()) {
+        this.error = true
+        this.errorMessage = 'All blocks must have an unique name'
+      }
+    },
+
+    verifyBlocksNamed() {
+      let allBlocksNamed = true
+      for (let i = 0; i < this.blocks.length; i++) {
+        if (this.blocks[i].name === '') {
+          allBlocksNamed = false
+          break
+        }
+      }
+      return allBlocksNamed
+    },
+
+    verifyNamed() {
+      return this.name !== ''
+    },
+
+    verifyUniqueNames() {
+      let allUniqueNames = true
+      for (let i = 0; i < this.blocks.length - 1; i++) {
+        for (let j = i + 1; i < this.blocks.length; j++) {
+          if (this.blocks[i].name === this.blocks[j].name) {
+            allUniqueNames = false
+            break
+          }
+        }
+        if (!allUniqueNames) {
+          break
+        }
+      }
+      return allUniqueNames
+    },
+  },
+
+  mounted() {
+    if (this.id > 0) {
+      this.$api.models.get({ id: this.id }).then(data => {
+        this.name = data.name
+        this.blocks = data.data.blocks
+      })
+    }
   },
 }
 </script>
