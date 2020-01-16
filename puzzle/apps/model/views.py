0a1692e68369e81e8bd3ed28fd32e62f77dd1f8b
Kremor Pärt <hello@kremor.xyz>
2020-01-16 12:46:47 -0600

    Update model api view

--- puzzle/apps/model/views.py
+++ puzzle/apps/model/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
+import json
 from puzzle.apps.model.models import Model
 from puzzle.apps.model.serializers import ModelSerializer
 from puzzle.utils.decorators import private
@@ -10,7 +11,20 @@ from rest_framework.parsers import JSONParser
 class ModelAPI(View):
 
     @method_decorator(private)
-    def get(self, req):
+    def get(self, request):
         models = Model.objects.all()
         serializer = ModelSerializer(models, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    @method_decorator(private)
+    def post(self, request, *args, **kwargs):
+        data = JSONParser().parse(request)
+        data['data'] = json.dumps(data.get('data', {}))
+        serializer = ModelSerializer(data=data)
+        if serializer.is_valid():
+            # serializer.save()
+            response = serializer.data
+            if response['data']:
+                response['data'] = json.loads(response['data'])
+            return JsonResponse(response)
+        return JsonResponse(serializer.errors, status=400)
