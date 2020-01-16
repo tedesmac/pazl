58d0b01a4437f1e90bf718db342318acf412458c
Kremor Pärt <hello@kremor.xyz>
2020-01-16 07:37:13 -0600

    Update editor workspace

--- assets/js/components/editor/workspace.vue
+++ assets/js/components/editor/workspace.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="workspace">
-    <div class="workspace-area">
+    <div class="workspace-area" :style="style">
       <slot />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    edit: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  computed: {
+    style() {
+      return {
+        padding: this.edit ? '0.5em' : '0',
+      }
+    },
+  },
+}
+</script>
