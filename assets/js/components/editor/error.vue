3654698e2b94aec94f4148c3edc6f2a13a484647
Kremor Pärt <hello@kremor.xyz>
2020-01-16 13:14:08 -0600

    Add error component

---
+++ assets/js/components/editor/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="error" v-if="show" @click="onClose">
+    <FontAwesomeIcon class="icon" :icon="faTimes" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  components: { FontAwesomeIcon },
+
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    faTimes: () => faTimes,
+  },
+
+  methods: {
+    onClose() {
+      this.$emit('close')
+    },
+  },
+}
+</script>
