79b79cc3b26d6304c7554f84c61770b944bf975b
Kremor Pärt <hello@kremor.xyz>
2020-01-17 07:59:34 -0600

    Add intial blocks

--- puzzle/assets/js/components/block/index.vue
+++ puzzle/assets/js/components/block/index.vue
@@ -5,5 +5,32 @@
 </template>
 
 <script>
-export default {}
+const ContainerBlock = import(
+  /* webpackChunkName: 'containerBlock' */
+  'components/block/container'
+)
+const HtmlBlock = import(
+  /* webpackChunkName: 'htmlBlock' */
+  'components/block/html'
+)
+const ImageBlock = import(
+  /* webpackChunkName: 'imageBlock' */
+  'components/block/image'
+)
+const LinkBlock = import(
+  /* webpackChunkName: 'linkBlock' */
+  'components/block/link'
+)
+const MarkdownBlock = import(
+  /* webpackChunkName: 'markdownBlock' */
+  'components/block/markdown'
+)
+const StringBlock = import(
+  /* webpackChunkName: 'stringBlock' */
+  'components/block/string'
+)
+
+export default {
+  components: { HtmlBlock, ImageBlock, LinkBlock, MarkdownBlock, StringBlock },
+}
 </script>
