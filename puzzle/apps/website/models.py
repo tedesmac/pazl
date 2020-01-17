be88174774e3acdbb6cd4c299b11c0a8d0776bea
Kremor Pärt <hello@kremor.xyz>
2020-01-17 17:30:31 -0600

    Add home_page field to Website

--- puzzle/apps/website/models.py
+++ puzzle/apps/website/models.py
@@ -5,6 +5,12 @@ class Website(models.Model):
     description = models.CharField(max_length=200, blank=True, null=False)
     logo = models.ImageField(upload_to='site', null=True, blank=True)
     name = models.CharField(max_length=50, blank=True, null=False)
+    home_page = models.ForeignKey(
+        'page.Page',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         db_table = 'puzzle_website'
