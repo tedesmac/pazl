d15b99944968292a18fcb9e55bdbd314973feaf8
Kremor Pärt <hello@kremor.xyz>
2020-01-17 07:45:41 -0600

    Update admin pages

--- puzzle/assets/js/components/admin/pages.vue
+++ puzzle/assets/js/components/admin/pages.vue
@@ -3,9 +3,19 @@
     <div class="section is-horizontal space-around">
       <h2>Pages</h2>
 
-      <button class="is-cyan">New</button>
+      <button class="is-cyan" @click="onNew">New</button>
     </div>
 
     <div class="section"></div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onNew() {
+      window.location = '/puzzle/editor/page/'
+    },
+  },
+}
+</script>
