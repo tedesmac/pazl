ae49d40f42ac079056284311129a669799f54098
Kremor Pärt <hello@kremor.xyz>
2020-01-20 11:19:19 -0600

    Add genId to client utils

--- puzzle/assets/js/utils/index.js
+++ puzzle/assets/js/utils/index.js
@@ -1,3 +1,14 @@
+const charMap = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+export const genId = (length = 8) => {
+  let id = ''
+  for (let i = 0; i < length; i++) {
+    const n = Math.floor(Math.random() * charMap.length)
+    id += charMap[n]
+  }
+  return id
+}
+
 export const routes = {
   admin: '/puzzle/admin/',
   api: '/puzzle/api/',
