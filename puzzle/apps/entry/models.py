78bfd7089b6ada4fb20fb5f4fa28a3e912d2d41e
Kremor Pärt <hello@kremor.xyz>
2020-01-21 13:44:28 -0600

    Add Entry model

--- puzzle/apps/entry/models.py
+++ puzzle/apps/entry/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from puzzle.utils.models import BasePageModel
 
-# Create your models here.
+
+class Entry(BasePageModel):
+    author = models.ForeignKey(
+        'user.User',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    model = models.ForeignKey(
+        'model.Model',
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'puzzle_entry'
