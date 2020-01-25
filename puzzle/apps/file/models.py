dfb023805b61939314e5610a15ba8e2cc88beb1d
Kremor Pärt <hello@kremor.xyz>
2020-01-25 12:43:22 -0600

    Add Image model

--- puzzle/apps/file/models.py
+++ puzzle/apps/file/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='img')
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = 'puzzle_image'
