1e721dd9de1fc88a7b10047ebf1a8f462ab1f577
Kremor Pärt <hello@kremor.xyz>
2020-01-28 12:17:31 -0600

    Update Image model

--- puzzle/apps/file/models.py
+++ puzzle/apps/file/models.py
@@ -1,9 +1,16 @@
+from django.conf import settings
 from django.db import models
+from os import path
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='img')
+    image = models.ImageField()
     name = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         db_table = 'puzzle_image'
+
+    @property
+    def url(self):
+        _, tail = path.split(self.image.name)
+        return '{}img/{}/'.format(settings.MEDIA_URL, tail)
