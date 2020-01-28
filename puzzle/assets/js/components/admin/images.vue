5c8907df1934590721ff64cbd8a72e878173191b
Kremor Pärt <hello@kremor.xyz>
2020-01-28 13:55:19 -0600

    Update admin images component

--- puzzle/assets/js/components/admin/images.vue
+++ puzzle/assets/js/components/admin/images.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    <div class="section is-horizontal space-around">
+    <div class="section is-horizontal space-between">
       <h2>Images</h2>
 
-      <button class="is-cyan">New</button>
+      <button class="is-cyan" @click="onNew">New</button>
     </div>
 
-    <div class="section"></div>
+    <div class="section">
+      <div class="image-list">
+        <div
+          v-for="(image, index) in images"
+          class="image-container"
+          :key="`image_${index}`"
+        >
+          <div
+            class="image"
+            :style="{ backgroundImage: `url(${image.url})` }"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [],
+    }
+  },
+
+  methods: {
+    onNew() {
+      this.$router.push({ name: 'upload' })
+    },
+  },
+
+  beforeMount() {
+    this.$api.images
+      .get()
+      .then(data => {
+        this.images = data
+      })
+      .catch(() => {})
+  },
+}
+</script>
