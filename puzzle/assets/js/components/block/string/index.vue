530116462c5784a105c3ccc31f359e851525709f
Kremor Pärt <hello@kremor.xyz>
2020-01-30 15:57:32 -0600

    Update string block

--- puzzle/assets/js/components/block/string/index.vue
+++ puzzle/assets/js/components/block/string/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    string
+  <div v-if="edit">
+    <input type="text" v-model="data" />
   </div>
+
+  <p v-else>{{ data }}</p>
 </template>
+
+<script>
+import { BlockMixin } from 'components/mixins'
+
+export default {
+  mixins: [BlockMixin],
+}
+</script>
