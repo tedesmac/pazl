87f551c576a307138afae437c1293ca6744be153
Kremor Pärt <hello@kremor.xyz>
2020-01-30 15:47:47 -0600

    Update markdown block

--- puzzle/assets/js/components/block/markdown/index.vue
+++ puzzle/assets/js/components/block/markdown/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    markdown
+  <div v-if="edit">
+    <MarkdownEditor :id="id" v-model="data" />
   </div>
+
+  <div v-else></div>
 </template>
+
+<script>
+import { BlockMixin } from 'components/mixins'
+
+const MarkdownEditor = () =>
+  import(
+    /* webpackChunkName: 'markdownEditor' */
+    './markdown-editor'
+  )
+
+export default {
+  mixins: [BlockMixin],
+
+  components: { MarkdownEditor },
+}
+</script>
