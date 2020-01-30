b72a1e26a049440a0c2e360a0f15b7c5e89a8611
Kremor Pärt <hello@kremor.xyz>
2020-01-30 08:31:12 -0600

    Update Toggle component

--- puzzle/assets/js/components/editor/page/toggle.vue
+++ puzzle/assets/js/components/editor/page/toggle.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mode-toggle">
-    <FontAwesomeIcon class="icon" :icon="faEdit" @click="current = 0" />
-    <FontAwesomeIcon class="icon" :icon="faMobileAlt" @click="current = 1" />
-    <FontAwesomeIcon class="icon" :icon="faTabletAlt" @click="current = 2" />
-    <FontAwesomeIcon class="icon" :icon="faDesktop" @click="current = 3" />
+    <FontAwesomeIcon class="icon" :icon="faEdit" @click="onChange(0)" />
+    <FontAwesomeIcon class="icon" :icon="faMobileAlt" @click="onChange(1)" />
+    <FontAwesomeIcon class="icon" :icon="faTabletAlt" @click="onChange(2)" />
+    <FontAwesomeIcon class="icon" :icon="faDesktop" @click="onChange(3)" />
     <div class="slider" :style="slider" />
   </div>
 </template>
@@ -37,6 +37,27 @@ export default {
     faEdit: () => faEdit,
     faMobileAlt: () => faMobileAlt,
     faTabletAlt: () => faTabletAlt,
+  },
+
+  methods: {
+    onChange(index) {
+      this.current = index
+
+      switch (index) {
+        case 0:
+          this.$emit('mode', 'edit')
+          break
+        case 1:
+          this.$emit('mode', 'mobile')
+          break
+        case 2:
+          this.$emit('mode', 'tablet')
+          break
+        case 3:
+          this.$emit('mode', 'desktop')
+          break
+      }
+    },
   },
 }
 </script>
