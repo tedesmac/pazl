34d07a0ef77f05135d5ec0ad51c6031ec69ccea4
Kremor Pärt <hello@kremor.xyz>
2020-02-11 16:07:10 -0600

    Update option setting

--- puzzle/assets/js/components/setting/option/index.vue
+++ puzzle/assets/js/components/setting/option/index.vue
@@ -1,5 +1,42 @@
 <template>
   <div>
-    option
+    {{ name }}:
+
+    <select v-model="value">
+      <option v-for="option in options" :key="option">{{ option }}</option>
+    </select>
   </div>
 </template>
+
+<script>
+import { SettingMixin } from '@/components/mixins'
+
+export default {
+  mixins: [SettingMixin],
+
+  computed: {
+    name() {
+      return this.setting._name
+    },
+
+    options() {
+      return this.setting.options
+    },
+
+    value: {
+      get() {
+        return this.setting.value
+      },
+
+      set(value) {
+        this.$store.commit('page/setBlockSetting', {
+          id: this.blockId,
+          name: this.name,
+          root: this.root,
+          value,
+        })
+      },
+    },
+  },
+}
+</script>
