f5a70afa4defa0215fa364b89a66b967db2c2782
Kremor Pärt <hello@kremor.xyz>
2020-02-11 15:14:19 -0600

    Update editor store

--- puzzle/assets/js/store/editor.js
+++ puzzle/assets/js/store/editor.js
@@ -2,10 +2,17 @@ export default {
   namespaced: true,
 
   state: () => ({
+    blockSettings: { data: {}, style: {} },
+    currentBlockId: '',
     edit: true,
   }),
 
   mutations: {
+    setBlockSettings(state, payload) {
+      state.currentBlockId = payload.id
+      state.blockSettings = payload.settings
+    },
+
     setEdit(state, value) {
       state.edit = value
     },
