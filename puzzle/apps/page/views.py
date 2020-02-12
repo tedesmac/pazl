3199d09497fa92541c3974b17a472a768f298334
Kremor Pärt <hello@kremor.xyz>
2020-02-12 13:24:17 -0600

    Use generic custom views in Page views

--- puzzle/apps/page/views.py
+++ puzzle/apps/page/views.py
@@ -1,12 +1,7 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views.generic import View
-import json
 from puzzle.apps.page.models import Page
 from puzzle.apps.page.serializers import PageSerializer
-from puzzle.utils.decorators import private
-from rest_framework.parsers import JSONParser
+from puzzle.utils.views import DetailAPIView, ListAPIView
 
 
 def home(req):
@@ -17,48 +12,15 @@ def page(req, *args, **kwargs):
     return render(req, 'page.html')
 
 
-class PageDetailAPI(View):
+class PageDetailAPI(DetailAPIView):
 
-    @method_decorator(private)
-    def delete(self, request, id):
-        pass
-
-    def get(self, request, id):
-        try:
-            page = Page.objects.get(id=id)
-        except Page.DoesNotExist:
-            return JsonResponse({}, status=400)
-        serializer = PageSerializer(page)
-        return JsonResponse(serializer.data)
-
-    @method_decorator(private)
-    def put(self, request, id):
-        try:
-            page = Page.objects.get(id=id)
-        except:
-            return JsonResponse({}, status=400)
-        data = JSONParser().parse(request)
-        serializer = PageSerializer(page, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+    class Meta:
+        model = Page
+        serializer = PageSerializer
 
 
-class PageListAPI(View):
+class PageListAPI(ListAPIView):
 
-    def get(self, request):
-        pages = Page.objects.all()
-        serializer = PageSerializer(pages, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    @method_decorator(private)
-    def post(self, request):
-        data = JSONParser().parse(request)
-        data['data'] = json.dumps(data.get('data', {}))
-        serializer = PageSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        print(serializer.errors)
-        return JsonResponse(serializer.errors, status=400)
+    class Meta:
+        model = Page
+        serializer = PageSerializer
