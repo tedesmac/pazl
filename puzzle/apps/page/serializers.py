76d274d082f94367b7bf84e564cf593df14ab918
Kremor Pärt <hello@kremor.xyz>
2020-02-12 13:22:12 -0600

    Use BaseSerializer for PageSerializer

--- puzzle/apps/page/serializers.py
+++ puzzle/apps/page/serializers.py
@@ -1,18 +1,16 @@
 from puzzle.apps.page.models import Page
-from rest_framework import serializers
+from puzzle.utils.serializers import BaseSerializer
 
 
-class PageSerializer(serializers.ModelSerializer):
-    data = serializers.JSONField(
-        required=True,
-        source='data_as_json',
-    )
-
+class PageSerializer(BaseSerializer):
     class Meta:
         model = Page
-        fields = ['id', 'data', 'description', 'name', 'parent', 'slug']
-
-    def create(self, validated_data):
-        validated_data['data'] = validated_data.get('data_as_json', '{}')
-        validated_data.pop('data_as_json', None)
-        return Page.objects.create(**validated_data)
+        fields = [
+            'id',
+            'data',
+            'description',
+            'name',
+            'parent',
+            'published',
+            'slug'
+        ]
