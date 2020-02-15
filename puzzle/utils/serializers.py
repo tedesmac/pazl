b0901839ac922ac3178b5b59b9ddb248d5050964
Kremor Pärt <hello@kremor.xyz>
2020-02-15 11:28:39 -0600

    Update BaseSerializer

--- puzzle/utils/serializers.py
+++ puzzle/utils/serializers.py
@@ -7,12 +7,23 @@ class BaseSerializer(serializers.ModelSerializer):
         source='data_as_json',
     )
 
+    def get_fields(self, *args, **kwargs):
+        fields = super().get_fields(*args, **kwargs)
+        request = self.context.get('request', None)
+        if request and getattr(request, 'method', None) == 'POST':
+            if 'name' in fields:
+                fields['name'].required = True
+        return fields
+
     def create(self, validated_data):
         validated_data['data'] = validated_data.get('data_as_json', '{}')
         validated_data.pop('data_as_json', None)
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
-        validated_data['data'] = validated_data.get('data_as_json', '{}')
+        validated_data['data'] = validated_data.get(
+            'data_as_json',
+            instance.data
+        )
         validated_data.pop('data_as_json', None)
         return super().update(instance, validated_data)
