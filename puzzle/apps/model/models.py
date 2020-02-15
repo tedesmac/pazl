d305c4ee72042fbe010618853f8bc98507c66b45
Kremor Pärt <hello@kremor.xyz>
2020-02-14 18:09:22 -0600

    Use base class BaseModel in Model

--- puzzle/apps/model/models.py
+++ puzzle/apps/model/models.py
@@ -1,26 +1,8 @@
 from django.db import models
-import json
+from puzzle.utils.models import BaseModel
 
 
-class Model(models.Model):
-    data = models.TextField()
-    name = models.CharField(max_length=50)
+class Model(BaseModel):
 
     class Meta:
         db_table = 'puzzle_model'
-
-    @property
-    def data_as_json(self):
-        if type(self.data) is dict:
-            return self.data
-        return json.loads(self.data)
-
-    @data_as_json.setter
-    def data_as_json(self, data: dict):
-        print(data)
-        self.data = json.dumps(data)
-
-    def save(self, *args, **kwargs):
-        if type(self.data) is dict:
-            self.data = json.dumps(self.data)
-        super().save(*args, **kwargs)
