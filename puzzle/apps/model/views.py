b5f5e153a883056c5dbd91a846b027fc9a405af3
Kremor Pärt <hello@kremor.xyz>
2020-02-14 18:13:56 -0600

    Use generic views for Model views

--- puzzle/apps/model/views.py
+++ puzzle/apps/model/views.py
@@ -1,54 +1,16 @@
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-from django.views.generic import View
-import json
 from puzzle.apps.model.models import Model
 from puzzle.apps.model.serializers import ModelSerializer
-from puzzle.utils.decorators import private
-from rest_framework.parsers import JSONParser
+from puzzle.utils.views import DetailAPIView, ListAPIView
 
 
-class ModelDetailAPI(View):
+class ModelDetailAPI(DetailAPIView):
 
-    @method_decorator(private)
-    def delete(self, request, id):
-        pass
+    class Meta:
+        model = Model
+        serializer = ModelSerializer
 
-    def get(self, request, id):
-        try:
-            model = Model.objects.get(id=id)
-        except Model.DoesNotExist:
-            return JsonResponse({}, status=400)
-        serializer = ModelSerializer(model)
-        return JsonResponse(serializer.data)
+class ModelListAPI(ListAPIView):
 
-    @method_decorator(private)
-    def put(self, request, id):
-        try:
-            model = Model.objects.get(id=id)
-        except Model.DoesNotExist:
-            return JsonResponse({}, status=404)
-        data = JSONParser().parse(request)
-        serializer = ModelSerializer(model, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-
-class ModelListAPI(View):
-
-    def get(self, request):
-        models = Model.objects.all()
-        serializer = ModelSerializer(models, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    @method_decorator(private)
-    def post(self, request, *args, **kwargs):
-        data = JSONParser().parse(request)
-        data['data'] = json.dumps(data.get('data', {}))
-        serializer = ModelSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+    class Meta:
+        model = Model
+        serializer = ModelSerializer
