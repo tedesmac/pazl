6684a426c6678a2ae9307180ded6608797ded0e4
Kremor Pärt <hello@kremor.xyz>
2020-02-16 17:58:00 -0600

    Add Block API views

--- puzzle/apps/block/views.py
+++ puzzle/apps/block/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from puzzle.apps.block.models import Block
+from puzzle.apps.block.serializers import BlockSerializer
+from puzzle.utils.views import DetailAPIView, ListAPIView
 
-# Create your views here.
+
+class BlockDetailAPI(DetailAPIView):
+
+    class Meta:
+        model = Block
+        serializer = BlockSerializer
+
+
+class BlockListAPI(ListAPIView):
+
+    class Meta:
+        model = Block
+        serializer = BlockSerializer
