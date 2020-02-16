45b994f52237edd424a120f8c031f15ef4a26c53
Kremor Pärt <hello@kremor.xyz>
2020-02-16 17:50:10 -0600

    Add Block model

--- puzzle/apps/block/models.py
+++ puzzle/apps/block/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from puzzle.utils.models import BaseModel
 
-# Create your models here.
+
+class Block(BaseModel):
+    model = models.ForeignKey(
+        'model.Model',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        db_table = "puzzle_block"
