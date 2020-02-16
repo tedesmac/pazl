2976d9d812ad7319429e1a25e815b884cdd149b7
Kremor Pärt <hello@kremor.xyz>
2020-02-16 17:57:48 -0600

    Add BlockSerializer

---
+++ puzzle/apps/block/serializers.py
@@ -0,0 +1,13 @@
+from puzzle.apps.block.models import Block
+from puzzle.utils.serializers import BaseSerializer
+
+
+class BlockSerializer(BaseSerializer):
+
+    class Meta:
+        model = Block
+        fields = [
+            'id',
+            'data',
+            'model',
+        ]
