389b168ff14ea59906fcaf80900f47613b917ddf
Kremor Pärt <hello@kremor.xyz>
2020-02-15 20:58:22 -0600

    Update entry editor

--- puzzle/assets/js/components/editor/entry.vue
+++ puzzle/assets/js/components/editor/entry.vue
@@ -4,11 +4,12 @@
 
     <Workspace class="entry-workspace">
       <div
-        v-for="(block, index) in modelBlocks"
+        v-for="(block, index) in blocks"
         class="entry-block"
-        :key="`block_${index}`"
+        :key="block.name"
       >
-        <label>{{ block.type }}</label>
+        <label>{{ block.name }}</label>
+        <Setting root="root" :setting="block" :type="block.type" />
       </div>
     </Workspace>
 
@@ -16,12 +17,12 @@
       <Collapsible class="form" string="Settings">
         <div class="field">
           <label>Title</label>
-          <input type="text" />
+          <input maxlength="50" type="text" v-model="name" />
         </div>
 
         <div class="field">
           <label>Description</label>
-          <textarea />
+          <textarea maxlength="200" v-model="description" />
         </div>
 
         <div class="field">
@@ -37,30 +38,39 @@
       <Collapsible class="form" string="Advance Settings" :show="false">
         <div class="field">
           <label>Slug</label>
-          <input type="text" />
+          <input maxlength="100" type="text" v-model="slug" />
         </div>
       </Collapsible>
     </Sidebar>
+
+    <modal height="auto" name="image-select" width="80%" :scrollable="true">
+      <ImageGallery @image="onImageSelected" />
+    </modal>
   </Editor>
 </template>
 
 <script>
 import BlockStore from '@/store/page'
-import Block from 'components/block'
-import { EditorMixin } from 'components/mixins'
+import Block from '@/components/block'
+import { EditorMixin } from '@/components/mixins'
+import Setting from '@/components/setting'
+import EditorStore from '@/store/editor'
+import PageStore from '@/store/page'
+
+const ImageGallery = () =>
+  import(
+    /* webpackChunkName: 'imageGallery' */
+    '@/components/misc/image-gallery'
+  )
 
 export default {
   mixins: [EditorMixin],
 
-  components: { Block },
+  components: { Block, ImageGallery, Setting },
 
   data() {
     return {
-      model: {
-        data: {
-          blocks: [],
-        },
-      },
+      imageSelectedCallbak: null,
     }
   },
 
@@ -72,27 +82,89 @@ export default {
   },
 
   computed: {
-    modelBlocks() {
-      return this.model.data.blocks
+    blocks() {
+      return this.$store.state.page.blocks
+    },
+
+    description: {
+      get() {
+        return this.$store.state.page.description
+      },
+
+      set(value) {
+        this.$store.commit('page/setDescription', value)
+      },
+    },
+
+    image: {
+      get() {
+        return this.$store.state.page.image
+      },
+
+      set(value) {
+        this.$store.commit('page/setImage', value)
+      },
+    },
+
+    name: {
+      get() {
+        return this.$store.state.page.name
+      },
+
+      set(value) {
+        this.$store.commit('page/setName', value)
+      },
+    },
+
+    slug: {
+      get() {
+        return this.$store.state.page.slug
+      },
+
+      set(value) {
+        this.$store.commit('page/setSlug', value)
+      },
     },
   },
 
-  created() {
-    if (this.modelId > 0) {
-      this.$api.models.get({ id: this.modelId }).then(data => {
-        this.model = data
-      })
-    } else {
-      window.location = '/puzzle/admin/'
-    }
+  methods: {
+    beforeImageGallery(event) {
+      this.imageSelectedCallbak = event.params.callback
+    },
 
-    if (this.id > 0) {
-      this.$api.entries
-        .get({ id: this.id })
-        .then(data => {})
-        .catch(() => {
-          window.location = `/puzzle/admin/entries/${this.model}`
+    onImageSelected(id) {
+      this.$modal.hide('image-select')
+    },
+
+    redirectToAdmin() {
+      window.location = '/puzzle/admin/'
+    },
+  },
+
+  beforeDestroy() {
+    this.$store.unregisterModule('editor')
+    this.$store.registerModule('page')
+  },
+
+  created() {
+    this.$store.registerModule('editor', EditorStore)
+    this.$store.registerModule('page', PageStore)
+
+    if (this.modelId > 0) {
+      this.$store
+        .dispatch('page/setupEntry', this.modelId)
+        .then(() => {
+          if (this.id > 0) {
+            this.$store.dispatch('page/fetchEntry', this.id).catch(() => {
+              window.location = `/puzzle/admin/entries/${this.model}`
+            })
+          }
         })
+        .catch(() => {
+          this.redirectToAdmin()
+        })
+    } else {
+      this.redirectToAdmin()
     }
   },
 }
