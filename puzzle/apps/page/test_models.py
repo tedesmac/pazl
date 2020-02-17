9063cd8f578f6fc82fba0006f8469c06f223b59c
Kremor Pärt <hello@kremor.xyz>
2020-02-17 13:38:51 -0600

    Update Page model tests

--- puzzle/apps/page/test_models.py
+++ puzzle/apps/page/test_models.py
@@ -1,4 +1,5 @@
 from puzzle.apps.page.models import Page
+from puzzle.apps.website.models import Website
 import pytest
 
 
@@ -6,7 +7,7 @@ import pytest
 def test_page_build_path_simple():
     page = Page.objects.create(name='Home')
     path = page.build_path()
-    assert path == '/home/'
+    assert path == 'home/'
 
 
 @pytest.mark.django_db
@@ -14,7 +15,7 @@ def test_page_build_path_two():
     parent = Page.objects.create(name='Parent')
     child = Page.objects.create(name='Child', parent=parent)
     path = child.build_path()
-    assert path == '/parent/child/'
+    assert path == 'parent/child/'
 
 
 @pytest.mark.django_db
@@ -23,4 +24,11 @@ def test_page_build_path_three():
     parent = Page.objects.create(name='Parent', parent=grandparent)
     child = Page.objects.create(name='Child', parent=parent)
     path = child.build_path()
-    assert path == '/grand-parent/parent/child/'
+    assert path == 'grand-parent/parent/child/'
+
+
+@pytest.mark.django_db
+def test_home_page_path():
+    page = Page.objects.create(name='test')
+    site = Website.objects.create(home_page=page)
+    assert page.path == ''
