413ad67013e4e1176f4bf882e2d8f9a6578bee72
Kremor Pärt <hello@kremor.xyz>
2020-02-17 13:38:08 -0600

    Update Page model

--- puzzle/apps/page/models.py
+++ puzzle/apps/page/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from puzzle.apps.website.models import Website
 from puzzle.utils.models import BasePageModel
 
 
@@ -14,9 +15,14 @@ class Page(BasePageModel):
         db_table = 'puzzle_page'
 
     def build_path(self):
+        home_page = Website.get_home_page()
+        if home_page:
+            if home_page.id == self.id:
+                self.slug = ''
+                return self.slug
         slug = super().build_path()
         if self.parent:
             slug = '{}{}/'.format(self.parent.build_path(), slug)
         else:
-            slug = '/{}/'.format(slug)
+            slug = '{}/'.format(slug)
         return slug
