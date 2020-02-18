eca87874583c0560492a2e66269fa921629739e1
Kremor Pärt <hello@kremor.xyz>
2020-02-17 19:41:23 -0600

    Update page component to render page blocks

--- puzzle/assets/js/components/page/index.vue
+++ puzzle/assets/js/components/page/index.vue
@@ -1,28 +1,30 @@
 <template>
-  <NotFound v-if="notFound" />
+  <NotFound v-if="error" />
 
   <div v-else class="__puzzle_page">
     <Menu />
 
     <div v-if="isEntry">Entry</div>
-    <div v-else>Page</div>
+
+    <Block
+      v-for="(block, index) in blocks"
+      :block="block.id"
+      :key="block.id"
+      :type="block.type"
+    />
   </div>
 </template>
 
 <script>
 import Block from '@/components/block'
+import PageStore from '@/store/page'
 import Menu from './menu'
+import { mapState } from 'vuex'
 
 const NotFound = () => import('@/components/404')
 
 export default {
   components: { Block, Menu, NotFound },
-
-  data() {
-    return {
-      notFound: false,
-    }
-  },
 
   props: {
     isEntry: {
@@ -31,9 +33,23 @@ export default {
     },
   },
 
+  computed: mapState({
+    blocks: state => state.page.blocks,
+
+    error: state => state.page.error,
+  }),
+
+  beforeDestroy() {
+    this.$store.unregisterModule('page')
+  },
+
   created() {
-    const path = window.location.pathname
-    console.log(path)
+    this.$store.registerModule('page', PageStore)
+    if (this.isEntry) {
+    } else {
+      const path = window.location.pathname.substring(1)
+      this.$store.dispatch('page/fetchPageByPath', path)
+    }
   },
 }
 </script>
