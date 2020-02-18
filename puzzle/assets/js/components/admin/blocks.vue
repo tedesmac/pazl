4dc0c2adf85b33399eff771af98ed5be51002d4a
Kremor Pärt <hello@kremor.xyz>
2020-02-17 19:40:30 -0600

    Update admin blocks

--- puzzle/assets/js/components/admin/blocks.vue
+++ puzzle/assets/js/components/admin/blocks.vue
@@ -2,10 +2,61 @@
   <div>
     <div class="section is-horizontal space-between">
       <h2>Blocks</h2>
-
-      <button class="is-cyan">New</button>
     </div>
 
-    <div class="section"></div>
+    <div
+      v-for="model in models"
+      class="section is-vertical"
+      :key="`model_block_section_${model.id}`"
+    >
+      <div class="is-full is-horizontal space-between">
+        <h2>{{ model.name }} Blocks</h2>
+        <button class="is-cyan">New</button>
+      </div>
+
+      <div></div>
+    </div>
+
+    <div class="section is-vertical">
+      <div class="is-full is-horizontal space-between">
+        <h2>Non Model Blocks</h2>
+        <button class="is-cyan">New</button>
+      </div>
+
+      <div></div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      blocks: {},
+    }
+  },
+
+  computed: mapState({
+    models: state => state.admin.models,
+  }),
+
+  created() {
+    this.$api.blocks.get().then(data => {
+      this.blocks = data.reduce(
+        (acc, b) => {
+          if (b.model) {
+            const model = this.models.reduce(
+              (acc, m) => (m.id === b.model ? m.name : acc),
+              'none'
+            )
+          }
+          return { ...acc, _noModel: [...acc._noModel, b] }
+        },
+        { _noModel: [] }
+      )
+    })
+  },
+}
+</script>
