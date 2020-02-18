ff7a63a012f25f5747bcb0a66f8808a38d3eb19b
Kremor Pärt <hello@kremor.xyz>
2020-02-18 07:02:31 -0600

    Update page component

--- puzzle/assets/js/components/page/index.vue
+++ puzzle/assets/js/components/page/index.vue
@@ -2,13 +2,27 @@
   <NotFound v-if="error" />
 
   <div v-else class="__puzzle_page">
-    <Menu />
+    <Menu :pages="menu" />
 
-    <div v-if="isEntry">Entry</div>
+    <div v-if="entryId && modelBlock">Model Block</div>
 
     <Block
+      v-else-if="entryId"
       v-for="(block, index) in blocks"
+      parent="root"
+      :block="block.name"
+      :index="index"
+      :isEntry="true"
+      :key="block.name"
+      :type="block.type"
+    />
+
+    <Block
+      v-else
+      v-for="block in blocks"
+      parent="root"
       :block="block.id"
+      :index="index"
       :key="block.id"
       :type="block.type"
     />
@@ -26,10 +40,17 @@ const NotFound = () => import('@/components/404')
 export default {
   components: { Block, Menu, NotFound },
 
+  data() {
+    return {
+      menu: [],
+      site: {},
+    }
+  },
+
   props: {
-    isEntry: {
-      type: Boolean,
-      default: false,
+    entryId: {
+      type: Number,
+      default: 0,
     },
   },
 
@@ -37,7 +58,48 @@ export default {
     blocks: state => state.page.blocks,
 
     error: state => state.page.error,
+
+    modelBlock: state => state.page.modelBlock,
   }),
+
+  methods: {
+    getPageChildren(id, pages) {
+      const children = pages.filter(p => p.parent === id)
+      const notChildren = pages.filter(p => !children.includes(p))
+      return pages.reduce((acc, p) => {
+        if (p.id === id) {
+          return [
+            ...acc,
+            {
+              children: this.getPageChildren(p.id, notChildren),
+              name: p.name,
+              url: `/puzzle/page/${p.id}/`,
+            },
+          ]
+        }
+        return acc
+      }, [])
+    },
+
+    fetchMenu() {
+      this.$api.pages.get({ published: 1 }).then(pages => {
+        const { home_page } = this.site
+        if (home_page) {
+          this.menu = this.getPageChildren(
+            home_page,
+            pages.filter(p => p.id !== home_page)
+          )
+        }
+      })
+    },
+
+    fetchSite() {
+      return this.$api.site.get().then(site => {
+        this.site = site
+        return Promise.resolve()
+      })
+    },
+  },
 
   beforeDestroy() {
     this.$store.unregisterModule('page')
@@ -45,11 +107,19 @@ export default {
 
   created() {
     this.$store.registerModule('page', PageStore)
-    if (this.isEntry) {
+    if (this.entryId) {
+      this.$store
+        .dispatch('page/fetchEntry', this.entryId)
+        .then(this.fetchModel)
     } else {
       const path = window.location.pathname.substring(1)
       this.$store.dispatch('page/fetchPageByPath', path)
     }
+    this.fetchSite()
+      .then(this.fetchMenu)
+      .catch(error => {
+        console.error('[page] =>', error)
+      })
   },
 }
 </script>
