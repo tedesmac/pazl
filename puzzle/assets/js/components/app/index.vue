19768f2a3783b26e8f0b35df048d5641a601f016
Kremor Pärt <hello@kremor.xyz>
2020-03-27 08:46:56 -0600

    Refactor app component

--- puzzle/assets/js/components/app/index.vue
+++ puzzle/assets/js/components/app/index.vue
@@ -1,3 +1,27 @@
 <template>
   <router-view />
 </template>
+
+<script>
+export default {
+  methods: {
+    fetchPages() {
+      this.$store.dispatch('fetchPages')
+    },
+
+    fetchSite() {
+      this.$store.dispatch('fetchSite')
+    },
+
+    fetchUser() {
+      this.$store.dispatch('fetchUser')
+    },
+  },
+
+  created() {
+    this.fetchPages()
+    this.fetchSite()
+    this.fetchUser()
+  },
+}
+</script>
