8d77bfad8dd9c4c1b358b4a4da225310429c018a
Kremor Pärt <hello@kremor.xyz>
2020-04-01 21:17:16 -0600

    Refactor user settings

--- puzzle/assets/js/components/admin/settings-users.vue
+++ puzzle/assets/js/components/admin/settings-users.vue
@@ -1,5 +1,52 @@
 <template>
   <div>
-    Settings Users
+    <div class="section is-horizontal space-between">
+      <div>
+        <h2>Users</h2>
+      </div>
+      <div>
+        <button class="is-cyan">New</button>
+      </div>
+    </div>
+
+    <div class="section is-vertical">
+      <entry
+        v-for="(user, index) in users"
+        :buttons="false"
+        :index="index"
+        :key="`user_${user.id}`"
+        :name="user.username"
+      >
+        <template #buttons>
+          {{ user.role }}
+        </template>
+      </entry>
+    </div>
   </div>
 </template>
+
+<script>
+import { AdminListingMixin } from '@/components/mixins'
+
+export default {
+  mixins: [AdminListingMixin],
+
+  data() {
+    return {
+      users: [],
+    }
+  },
+
+  methods: {
+    fetchUsers() {
+      this.$api.users.get().then(users => {
+        this.users = users
+      })
+    },
+  },
+
+  created() {
+    this.fetchUsers()
+  },
+}
+</script>
