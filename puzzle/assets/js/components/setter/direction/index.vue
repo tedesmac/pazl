b38b356b7d66673f5cb6862349c5b9e167d2d90f
Kremor Pärt <hello@kremor.xyz>
2020-04-07 13:31:46 -0500

    Refactor Direction Setter

--- puzzle/assets/js/components/setter/direction/index.vue
+++ puzzle/assets/js/components/setter/direction/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    Not implemented
+    <label>Direction:</label>
+    <select v-model="direction">
+      <option value="horizontal">Horizontal</option>
+      <option value="vertical">Vertical</option>
+    </select>
   </div>
 </template>
+
+<script>
+import { SetterMixin } from '@/components/mixins'
+
+export default {
+  mixins: [SetterMixin],
+
+  computed: {
+    direction: {
+      get() {},
+
+      set(direction) {
+        this.block = {
+          ...this.block,
+          data: {
+            ...this.block.data,
+            direction,
+          },
+        }
+      },
+    },
+  },
+}
+</script>
