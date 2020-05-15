04a3a092bd5e73bcdde6d8c6866d7ae22dc2e71d
Kremor Pärt <hello@kremor.xyz>
2020-05-15 08:12:42 -0500

    Hide editor's sidebar in preview mode

--- puzzle/assets/js/components/editor/sidebar.vue
+++ puzzle/assets/js/components/editor/sidebar.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="sidebar">
+  <div class="sidebar" :style="style">
     <slot />
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState({
+    style: state => {
+      const { mode } = state.editor
+      if (mode === 'edit') {
+        return {}
+      }
+      return {
+        transform: 'translateX(100%)',
+      }
+    },
+  }),
+}
+</script>
