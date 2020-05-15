37c686e8ee337519d964cf2b1e1c17e01b843f63
Kremor Pärt <hello@kremor.xyz>
2020-05-15 14:47:23 -0500

    Update image block

--- puzzle/assets/js/components/block/image/index.vue
+++ puzzle/assets/js/components/block/image/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <img class="__puzzle_image" :alt="alt" :src="src" />
+  <div>
+    <img class="__puzzle_image" :alt="alt" :src="src" />
+    <FontAwesomeIcon v-if="edit" class="edit-icon" :icon="faImage" />
+  </div>
 </template>
 
 <script>
-import { BlockMixin } from 'components/mixins'
+import { BlockMixin } from '@/components/mixins'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
   mixins: [BlockMixin],
+
+  components: { FontAwesomeIcon },
 
   computed: {
     alt() {
@@ -14,8 +21,11 @@ export default {
     },
 
     src() {
-      return this.getData('src')
+      const src = this.getData('src')
+      return src
     },
+
+    faImage: () => faImage,
   },
 }
 </script>
