627b8cd436cf492bce3b64ec2b67c694383f3bd3
Kremor Pärt <hello@kremor.xyz>
2020-06-03 10:22:12 -0500

    Add tests for date_published

--- puzzle/apps/page/test_page.py
+++ puzzle/apps/page/test_page.py
@@ -23,15 +23,6 @@ from puzzle.apps.website.signals import *
 
 
 @pytest.mark.django_db
-def test_page_data_property():
-    page = Page.objects.create()
-    assert page.data == {}
-    page.data = { 'test': 'test' }
-    assert page._data == '{"test":"test"}'
-    assert isinstance(page.data, dict)
-
-
-@pytest.mark.django_db
 def test_page_build_path_simple():
     page = Page.objects.create(name='Home')
     path = page.build_path()
@@ -53,6 +44,27 @@ def test_page_build_path_three():
     child = Page.objects.create(name='Child', parent=parent)
     path = child.build_path()
     assert path == '/grand-parent/parent/child'
+
+
+@pytest.mark.django_db
+def test_page_date_published_is_none():
+    page = Page.objects.create(name='Some Page')
+    assert page.date_published == None
+
+
+@pytest.mark.django_db
+def test_page_date_published_not_none():
+    page = Page.objects.create(name='Some Page', published=True)
+    assert page.date_published != None
+
+
+@pytest.mark.django_db
+def test_page_data_property():
+    page = Page.objects.create()
+    assert page.data == {}
+    page.data = { 'test': 'test' }
+    assert page._data == '{"test":"test"}'
+    assert isinstance(page.data, dict)
 
 
 @pytest.mark.django_db
